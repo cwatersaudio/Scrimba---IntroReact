671739562242a8aac4405e19c6394e428b66fcd0
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2023-10-04 10:34:48 -0400

    matching figma file

--- ReactInfoPage/ReactInfoPage/src/components/MainPage.jsx
+++ ReactInfoPage/ReactInfoPage/src/components/MainPage.jsx
@@ -25,11 +25,11 @@ const MainPage = () => {
             <div id="facts-content">
                 <h1>Fun facts about React</h1>
                 <ul>
-                    <li><span>Was first released in 2013</span></li>
-                    <li><span>Was originally created by Jordan Walke</span></li>
-                    <li><span>Has well over 100k stars on GitHub</span></li>
-                    <li><span>Is maintained by Facebook</span></li>
-                    <li><span>Powers thousands of enterprise apps, <br></br>including mobile apps</span></li>
+                    <li ClassName="list-item"><span>Was first released in 2013</span></li>
+                    <li ClassName="list-item"><span>Was originally created by Jordan Walke</span></li>
+                    <li ClassName="list-item"><span>Has well over 100k stars on GitHub</span></li>
+                    <li ClassName="list-item"><span>Is maintained by Facebook</span></li>
+                    <li ClassName="list-item"><span>Powers thousands of enterprise apps, <br></br>including mobile apps</span></li>
                 </ul>
             </div>
         </div>   
