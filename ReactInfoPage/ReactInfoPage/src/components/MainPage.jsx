84e07c6a86a110acbdc5a1ced0f5290fae2bca3b
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2023-10-04 20:33:33 -0400

    figured out logo and bullets

--- ReactInfoPage/ReactInfoPage/src/components/MainPage.jsx
+++ ReactInfoPage/ReactInfoPage/src/components/MainPage.jsx
@@ -23,25 +23,22 @@ const MainPage = () => {
         <>
         <div id="facts-container">
             <div id="facts-content">
-                <h1>Fun facts about React</h1>
-                 <div id="fact-list">
-                     {/* <svg xmlns="http://www.w3.org/2000/svg" width="10" height="161" viewBox="0 0 10 161" fill="none">
-                        <circle cx="4.80333" cy="4.80333" r="4.80333" fill="#61DAFB"/>
-                        <circle cx="4.80333" cy="43.8033" r="4.80333" fill="#61DAFB"/>
-                        <circle cx="4.80333" cy="80.8033" r="4.80333" fill="#61DAFB"/>
-                        <circle cx="4.80333" cy="117.803" r="4.80333" fill="#61DAFB"/>
-                        <circle cx="4.80333" cy="155.803" r="4.80333" fill="#61DAFB"/>
-                    </svg> */}
-                    <ul>
-                        <li ClassName="list-item"><span>Was first released in 2013</span></li>
-                        <li ClassName="list-item"><span>Was originally created by Jordan Walke</span></li>
-                        <li ClassName="list-item"><span>Has well over 100k stars on GitHub</span></li>
-                        <li ClassName="list-item"><span>Is maintained by Facebook</span></li>
-                        <li ClassName="list-item"><span>Powers thousands of enterprise apps, including mobile apps</span></li>
-                    </ul>
-                    
-                </div>    
+                    <h1>Fun facts about React</h1>
+                    <div id="fact-list">
+                        <ul>
+                            <li ClassName="list-item"><span>Was first released in 2013</span></li>
+                            <li ClassName="list-item"><span>Was originally created by Jordan Walke</span></li>
+                            <li ClassName="list-item"><span>Has well over 100k stars on GitHub</span></li>
+                            <li ClassName="list-item"><span>Is maintained by Facebook</span></li>
+                            <li ClassName="list-item"><span>Powers thousands of enterprise apps, including mobile apps</span></li>
+                        </ul>
+                        
+                    </div>  
             </div>
+            {/* <div id="bgd-logo">
+                    <img src="./src/assets/Group.svg" alt="react logo background" />
+            </div>   */}
+            
         </div>   
         </>
     )
