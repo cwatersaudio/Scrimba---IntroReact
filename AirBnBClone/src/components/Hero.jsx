1448c485c78fb0a84ab17cdaa75b975ca948e94f
Chris Waters <chriswatersaudio@gmail.com>
2023-10-09 12:14:46 -0400

    working on hero section

---
+++ AirBnBClone/src/components/Hero.jsx
@@ -0,0 +1,18 @@
+function Hero () {
+    return (
+        <>
+            <div id="hero-container">
+                <img src="./src/assets/photo-grid.png" alt="photos of experiences" id="static-collage"/>
+                
+                <div id="hero-text-container">
+                <h1>Online Experiences</h1>
+                <h4>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</h4>
+                </div>
+            </div>
+        </>
+    )
+
+
+}
+
+export default Hero
