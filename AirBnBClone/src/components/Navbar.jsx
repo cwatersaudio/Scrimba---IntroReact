2a8d936b2562303743ee63378b2a05a13ffebb21
Chris Waters <chriswatersaudio@gmail.com>
2023-10-09 11:42:12 -0400

    added navbar

---
+++ AirBnBClone/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar ()  {
+    return(
+        <>
+            <div id="nav-container">
+            <img src="./src/assets/airbnb-logo.png" alt="AirBnB Logo" id="bnb-logo"/>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
