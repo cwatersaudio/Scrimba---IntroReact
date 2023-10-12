7d31c6c576faea07fa0bad717febe0daf4eb40a4
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2023-10-11 23:14:28 -0400

    building

--- ShoppingList - React/src/components/Input.jsx
+++ ShoppingList - React/src/components/Input.jsx
@@ -1,11 +1,53 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-df319-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+
 function inputField () {
+    const app = initializeApp(appSettings)
+    const database = getDatabase(app)
+    const shoppingListInDB = ref(database, "shoppingList")
+
+    const inputFieldEl = document.getElementById("input-field")
+    const addButtonEl = document.getElementById("add-button")
+    const shoppingListEl = document.getElementById("shopping-list")
+    
+    function buttonAction() {
+        let inputValue = inputFieldEl.value
+        
+        push(shoppingListInDB, inputValue)
+        
+        clearInputFieldEl()
+    
+        appendItemToShoppingListEl(inputValue)
+    }
+    
+    function clearInputFieldEl() {
+        inputFieldEl.value = ""
+    }
+    
+    function appendItemToShoppingListEl(itemValue) {
+        shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+    }
+
+    onValue(shoppingListInDB, function(snapshot) {
+        let itemsArray = Object.values(snapshot.val())
+        
+        // Challenge: Write a for loop to iterate on itemsArray and console log each item
+    })
+
     return (
+        
+        
         <>
             <input type="text" id="input-field" placeholder="Bread"></input>
-            <button id="add-button">Add to cart</button>
+            <button id="add-button" onClick={buttonAction}>Add to cart</button>
+            <ul id="shopping-list"></ul>
         </>
 
     )
 }
 
-export default inputField
---
+++ ShoppingList - React/src/javajava.js
@@ -0,0 +1,38 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-df319-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    
+    clearInputFieldEl()
+
+    appendItemToShoppingListEl(inputValue)
+})
+
+onValue(shoppingListInDB, function(snapshot) {
+    let itemsArray = Object.values(snapshot.val())
+    
+    // Challenge: Write a for loop to iterate on itemsArray and console log each item
+})
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+export function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
