541a091b7249bf6f4f4acf108f729da4c12a8d81
Chris Waters <chriswatersaudio@gmail.com>
2023-10-25 12:57:05 -0400

    initial work on Header

--- MemeGen/src/App.jsx
+++ MemeGen/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1>Hello! </h1>
+      <main>
+        <Header />
+
+
+      </main>
     </>
   )
 }
---
+++ MemeGen/src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header () {
+    return (
+        <nav id="header-navbar">
+            <div>
+            <img src="./src/assets/Troll Face.png" alt="meme logo" />
+            <span id="app-title" className = "bold">Chris's Meme Generator</span>
+            </div>
+            <span id="app-info">Adapted from Scrimba Module 3</span>
+
+        </nav>
+
+
+
+
+    )
+}
