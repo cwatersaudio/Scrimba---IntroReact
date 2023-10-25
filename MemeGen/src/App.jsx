05c4796f3a8154fb703378e3cc4104122f9a49f9
Chris Waters <chriswatersaudio@gmail.com>
2023-10-25 09:43:39 -0400

    Initial project setup

--- MemeGen/src/App.jsx
+++ MemeGen/src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      
+      <h1>Hello! </h1>
     </>
   )
 }
