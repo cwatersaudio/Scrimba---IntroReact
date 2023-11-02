89c8cd5eea119a3896888cc77d77d38c5a1b3e83
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2023-11-01 22:34:07 -0400

    refactor state

--- MemeGen/src/components/Input.jsx
+++ MemeGen/src/components/Input.jsx
@@ -1,20 +1,40 @@
 import MArray from '../memesData.js'
 import React from 'react'
 
-
+ /**
+     * Challenge: Update our state to save the meme-related
+     * data as an object called `meme`. It should have the
+     * following 3 properties:
+     * topText, bottomText, randomImage.
+     * 
+     * The 2 text states can default to empty strings for now,
+     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+     * 
+     * Next, create a new state variable called `allMemeImages`
+     * which will default to `memesData`, which we imported above
+     * 
+     * Lastly, update the `getMemeImage` function and the markup 
+     * to reflect our newly reformed state object and array in the
+     * correct way.
+     */
 
 export default function Input () {
-    const [memeURL, setMemeURL] = React.useState("")
+    const [currentMeme, setMeme] = React.useState({
+            topText: "",
+            bottomText:"",
+            randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
     let imgURL, memeHeight, memeWidth;
 
     function getImage () {
             let randIndex = Math.floor(Math.random() * 99);
             let randMeme = MArray.data.memes[randIndex]
             let {name,url,width,height} = randMeme
-            setMemeURL(url)
-            document.getElementById("meme").style.setProperty('--meme-height',height+'px')
-            document.getElementById("meme").style.setProperty('--meme-width',width+'px')
-
+            setMeme(prevMeme => ({
+                ...prevMeme,
+                randomImage: url
+            })
+            )
         }
 
     
@@ -33,7 +53,7 @@ export default function Input () {
             <button id="meme-button" type="button" onClick={getImage}>Get a new meme image  🖼</button>
         </form>
         <img 
-        src={memeURL}
+        src={currentMeme.randomImage}
         alt="meme" 
         className = "meme--image"
         id="meme"
